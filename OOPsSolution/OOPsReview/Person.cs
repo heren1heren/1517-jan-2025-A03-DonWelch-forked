37c78273c20334539a9cb3212dec5450cd96be4d
Don Welch <dwelch@nait.ca>
2025-01-21 09:46:43 -0700

    create varous tests for TDD demo of creating Person class

---
+++ OOPsSolution/OOPsReview/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsReview
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ResidentAddress Address { get; set; }
+        public List<Employment> EmploymentPositions { get; set; }
+
+        public Person()
+        {
+            FirstName = "Unknown";
+            LastName = "Unknown";
+            //Address = null;
+            EmploymentPositions = new List<Employment>();
+        }
+
+        public Person(string firstname, string lastname, ResidentAddress address,
+                        List<Employment> employments)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            Address = address;
+            if (employments != null)
+                EmploymentPositions = employments;
+            else
+                EmploymentPositions = new List<Employment>();
+        }
+    }
+}
--- OOPsSolution/TDDUnitTesting/Person_Should.cs
+++ OOPsSolution/TDDUnitTesting/Person_Should.cs
@@ -7,6 +7,75 @@ namespace TDDUnitTesting
     {
         #region Constructors
         #region Successful
+        //the [Fact] unit test executes once
+        //without the [Fact] annotation, the method is NOT considered a unit test
+        [Fact]
+        public void Successfully_Create_An_Instance_Using_The_Default_Constructor()
+        {
+            //Arrange (this is the setup of values needed for doing the test)
+            string expectedFirstName = "Unknown";
+            string expectedLastName = "Unknown";
+            int expectedEmploymentPositionsCount = 0;
+
+            //Act (this is the action that is under testing)
+            //sut (subject under test)
+            Person sut = new Person();
+
+            //Assert (checks the results of the Act against expected values
+            sut.FirstName.Should().Be(expectedFirstName);
+            sut.LastName.Should().Be(expectedLastName);
+            sut.Address.Should().BeNull();
+            sut.EmploymentPositions.Count.Should().Be(expectedEmploymentPositionsCount);
+
+        }
+
+        [Fact]
+        public void Successfully_Create_An_Instance_Using_Greedy_Constructor_No_Address_Employment()
+        {
+            //Arrange (this is the setup of values needed for doing the test)
+            string expectedFirstName = "Don";
+            string expectedLastName = "Welch";
+            int expectedEmploymentPositionsCount = 0;
+
+            //Act (this is the action that is under testing)
+            //sut (subject under test)
+            Person sut = new Person("Don","Welch",null,null);
+
+            //Assert (checks the results of the Act against expected values
+            sut.FirstName.Should().Be(expectedFirstName);
+            sut.LastName.Should().Be(expectedLastName);
+            sut.Address.Should().BeNull();
+            sut.EmploymentPositions.Count.Should().Be(expectedEmploymentPositionsCount);
+        }
+
+        [Fact]
+        public void Successfully_Create_An_Instance_Using_Greedy_Constructor_With_Address_Employment()
+        {
+            //Arrange (this is the setup of values needed for doing the test)
+            string expectedFirstName = "Don";
+            string expectedLastName = "Welch";
+            ResidentAddress expectedAddress = new ResidentAddress(123, "Maple St,",
+                                        "Edmonton", "AB", "T6Y8U9");
+            Employment one = new Employment("PG I", SupervisoryLevel.TeamMember,
+                                                DateTime.Parse("2013/10/10"), 6.5);
+            Employment two = new Employment("PG II", SupervisoryLevel.TeamMember,
+                                               DateTime.Parse("2020/04/10"));
+            List<Employment> expectedEmployments = new List<Employment>();
+            expectedEmployments.Add(one);
+            expectedEmployments.Add(two);
+            int expectedEmploymentPositionsCount = 2;
+
+            //Act (this is the action that is under testing)
+            //sut (subject under test)
+            Person sut = new Person("Don", "Welch", expectedAddress, expectedEmployments);
+
+            //Assert (checks the results of the Act against expected values
+            sut.FirstName.Should().Be(expectedFirstName);
+            sut.LastName.Should().Be(expectedLastName);
+            sut.Address.Should().Be(expectedAddress);
+            sut.EmploymentPositions.Count.Should().Be(expectedEmploymentPositionsCount);
+            sut.EmploymentPositions.Should().ContainInConsecutiveOrder(expectedEmployments);
+        }
         #endregion
         #region Exception Testing
         #endregion
