148e62165fe0e20b6e1266753a45f093070fe746
Don Welch <dwelch@nait.ca>
2025-01-21 08:47:57 -0700

    setup xUnit testing project to demo TDD using Person class

---
+++ OOPsSolution/TDDUnitTesting/Person_Should.cs
@@ -0,0 +1,21 @@
+using OOPsReview;
+using FluentAssertions;
+
+namespace TDDUnitTesting
+{
+    public class Person_Should
+    {
+        #region Constructors
+        #region Successful
+        #endregion
+        #region Exception Testing
+        #endregion
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Methods
+        #endregion
+    }
+}
