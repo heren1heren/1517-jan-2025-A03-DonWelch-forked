15a7aabd089b9e6d531bdfab9b6644c6fd62f3f4
Don Welch <dwelch@nait.ca>
2025-03-11 09:48:59 -0600

    complete table, demo using nav property to obtain data from secondary table for display

--- CSSolution/WestWindSystem/BLL/ShipmentServices.cs
+++ CSSolution/WestWindSystem/BLL/ShipmentServices.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 #region Additonal Namespaces
 using WestWindSystem.DAL;
 using WestWindSystem.Entities;
+using Microsoft.EntityFrameworkCore;
 #endregion
 
 
@@ -25,9 +27,27 @@ namespace WestWindSystem.BLL
         #endregion
         #region Services
         //retrieve all shipments for a specified month
+
+        // Used to navigate on the ShipmentQuery page to the Shipper talble
+        //  Technique A
+        //public List<Shipment> Shipment_GetByYearAndMonth(int year, int month)
+        //{
+        //    IEnumerable<Shipment> info = _context.Shipments
+        //                                         .Where(s => s.ShippedDate.Year == year
+        //                                                  && s.ShippedDate.Month == month)
+        //                                         .OrderBy(s => s.ShippedDate);
+        //    return info.ToList();
+        //}
+
+        //This uses the technique (b) discussed on the ShipmentTable page
+        //note there is a required using class, see Additional namespaces above.
+        //uses the .Include method to add navigational instances to the return record
+        //note the predicate uses the virtual navigational property of the Shipment entity
+        //This will include the associated record from the Shippers table (parent) for the shipment record (child)
         public List<Shipment> Shipment_GetByYearAndMonth(int year, int month)
         {
             IEnumerable<Shipment> info = _context.Shipments
+                                                 .Include(s => s.ShipViaNavigation)
                                                  .Where(s => s.ShippedDate.Year == year
                                                           && s.ShippedDate.Month == month)
                                                  .OrderBy(s => s.ShippedDate);
